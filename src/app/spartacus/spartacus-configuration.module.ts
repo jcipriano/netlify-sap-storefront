3ae3fa6da14f7e7b7cedce6f2c486ad221982a24
jcipriano <>
2023-03-31 10:49:46 -0700

    First commit

---
+++ src/app/spartacus/spartacus-configuration.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { translationChunksConfig, translations } from "@spartacus/assets";
+import { FeaturesConfig, I18nConfig, OccConfig, provideConfig, SiteContextConfig } from "@spartacus/core";
+import { defaultCmsContentProviders, layoutConfig, mediaConfig } from "@spartacus/storefront";
+
+@NgModule({
+  declarations: [],
+  imports: [
+  ],
+  providers: [provideConfig(layoutConfig), provideConfig(mediaConfig), ...defaultCmsContentProviders, provideConfig(<OccConfig>{
+    backend: {
+      occ: {
+        baseUrl: 'https://api.c65i60w-esaustral1-s2-public.model-t.cc.commerce.ondemand.com',
+      }
+    },
+  }), provideConfig(<SiteContextConfig>{
+    context: {},
+  }), provideConfig(<I18nConfig>{
+    i18n: {
+      resources: translations,
+      chunks: translationChunksConfig,
+      fallbackLang: 'en'
+    },
+  }), provideConfig(<FeaturesConfig>{
+    features: {
+      level: '5.2'
+    }
+  })]
+})
+export class SpartacusConfigurationModule { }
